f37ad17dc18e0e462e1c7b440300859e851599b6
Tom Graham <tgraham1998@outlook.com>
2024-11-12 14:54:37 +0100

    action tree plotting with arrows

--- plotter.py
+++ plotter.py
@@ -93,7 +93,7 @@ def plot_k_weights(k_weights, ax, title=None):
 
 
 ## plot action tree
-def plot_action_tree(tree_q, start, goal):
+def plot_action_tree(tree_q, start, goal, action_arrow = True, ax=None):
     n_rows, n_cols, _ = tree_q.shape
 
     # Extract the action values for each direction directly from tree_q
@@ -111,7 +111,16 @@ def plot_action_tree(tree_q, start, goal):
     ## determine the best action at each state 
     best_actions = np.argmax(tree_q, axis=2)
 
-    fig, ax = plt.subplots(figsize=(n_rows*4, n_rows*1.2))
+    ## switch 0 and 2 to match the plot
+    # best_actions = np.where(best_actions == 0, -1, best_actions)
+    # best_actions = np.where(best_actions == 2, 0, best_actions)
+    # best_actions = np.where(best_actions == -1, 2, best_actions)
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(n_rows*4, n_rows*1.2))
+    else:
+        fig = ax.figure
+
     ax.set_ylim(n_rows, 0)
     tripcolor = quatromatrix(left, top, right, bottom, grid_size=(n_rows, n_cols), ax=ax,
                              triplotkw={"color": "k", "lw": 1},
@@ -119,38 +128,81 @@ def plot_action_tree(tree_q, start, goal):
 
     ax.margins(0)
     ax.set_aspect("equal")
-    fig.colorbar(tripcolor)
+    # fig.colorbar(tripcolor, ax=ax)
 
     # Function to round to 2 significant figures
-    # round_to_sigfigs = lambda x: f"{x:.2g}"
     round_to_sigfigs = lambda x: f"{x:.2f}"
 
     # Plot values on the grid with 2 significant figures, with the best action in bold and italics
+    size = 9
     for i, (xi, yi) in enumerate(top_value_positions):
-        plt.text(xi, yi, round_to_sigfigs(top.flatten()[i]), size=9, color="w", weight="bold" if best_actions.flatten()[i] == 2 else "normal", style="italic" if best_actions.flatten()[i] == 2 else "normal")
+        ax.text(xi, yi, round_to_sigfigs(top.flatten()[i]), size=size, color="w", weight="bold" if best_actions.flatten()[i] == 2 else "normal", style="italic" if best_actions.flatten()[i] == 0 else "normal")
     for i, (xi, yi) in enumerate(right_value_positions):
-        plt.text(xi, yi, round_to_sigfigs(right.flatten()[i]), size=9, color="w", weight="bold" if best_actions.flatten()[i] == 1 else "normal", style="italic" if best_actions.flatten()[i] == 1 else "normal")
+        ax.text(xi, yi, round_to_sigfigs(right.flatten()[i]), size=size, color="w", weight="bold" if best_actions.flatten()[i] == 1 else "normal", style="italic" if best_actions.flatten()[i] == 1 else "normal")
     for i, (xi, yi) in enumerate(left_value_positions):
-        plt.text(xi, yi, round_to_sigfigs(left.flatten()[i]), size=9, color="w", weight="bold" if best_actions.flatten()[i] == 3 else "normal", style="italic" if best_actions.flatten()[i] == 3 else "normal")
+        ax.text(xi, yi, round_to_sigfigs(left.flatten()[i]), size=size, color="w", weight="bold" if best_actions.flatten()[i] == 3 else "normal", style="italic" if best_actions.flatten()[i] == 3 else "normal")
     for i, (xi, yi) in enumerate(bottom_value_positions):
-        plt.text(xi, yi, round_to_sigfigs(bottom.flatten()[i]), size=9, color="w", weight="bold" if best_actions.flatten()[i] == 0 else "normal", style="italic" if best_actions.flatten()[i] == 0 else "normal")
+        ax.text(xi, yi, round_to_sigfigs(bottom.flatten()[i]), size=size, color="w", weight="bold" if best_actions.flatten()[i] == 0 else "normal", style="italic" if best_actions.flatten()[i] == 2 else "normal")
 
+    ## Plot arrows for the best action at each state
+    if action_arrow:
+
+        ## calculate the max(Q) route from start to goal
+        current = start
+        path = [current]
+        stuck = False
+        while np.array_equal(current, goal) == False and stuck == False:
+            i, j = current
+            action = best_actions[i, j]
+            if action == 0:
+                current = np.clip((i + 1, j), 0, n_rows-1)
+            elif action == 1:
+                current = np.clip((i, j + 1), 0, n_cols-1)
+            elif action == 2:
+                current = np.clip((i - 1, j), 0, n_rows-1)
+            elif action == 3:
+                current = np.clip((i, j - 1), 0, n_cols-1)
+            path.append(current)
+
+            ## check if the current state is already in the path
+            for p in path[:-1]:
+                if np.array_equal(p, current):
+                    stuck = True
+                    print('Stuck in a loop')
+
+
+        ## plot the max arrow for all states
+        for i in range(n_rows):
+            for j in range(n_cols):
+                if best_actions[i, j] == 2:
+                    ax.arrow(j + 0.5, i + 0.5, 0, -0.3, head_width=0.1, head_length=0.1, fc="k", ec="k")
+                elif best_actions[i, j] == 1:
+                    ax.arrow(j + 0.5, i + 0.5, 0.3, 0, head_width=0.1, head_length=0.1, fc="k", ec="k")
+                elif best_actions[i, j] == 0:
+                    ax.arrow(j + 0.5, i + 0.5, 0, 0.3, head_width=0.1, head_length=0.1, fc="k", ec="k")
+                elif best_actions[i, j] == 3:
+                    ax.arrow(j + 0.5, i + 0.5, -0.3, 0, head_width=0.1, head_length=0.1, fc="k", ec="k")
 
     ## set ticks as numbers 0-N
-    ticks = np.linspace(0.5, n_rows-0.5, n_rows)
-    labels = np.arange(n_rows)
-    ax.set_xticks(ticks, labels)
-    ax.set_yticks(ticks, labels)
+    # ticks = np.linspace(0.5, n_rows-0.5, n_rows)
+    # labels = np.arange(n_rows)
+    # ax.set_xticks(ticks, labels)
+    # ax.set_yticks(ticks, labels)
+    ax.set_xticks([])
+    ax.set_yticks([])
 
     # Mark the start and goal positions with circles
     start_x, start_y = start
     goal_x, goal_y = goal
     ax.plot(start_y + 0.5, start_x + 0.5, 'ro', markersize=10, label="Start")
     ax.plot(goal_y + 0.5, goal_x + 0.5, 'go', markersize=10, label="Goal")
-    # ax.legend(loc="upper right")
 
+    ## plot the max path
+    for i in range(len(path)-1):
+        ax.plot([path[i][1]+0.5, path[i+1][1]+0.5], [path[i][0]+0.5, path[i+1][0]+0.5], 'k-', linewidth=3, color='green')
 
     return ax
+
 def quatromatrix(left, bottom, right, top, grid_size, ax=None, triplotkw={}, tripcolorkw={}):
     if not ax: 
         ax = plt.gca()
