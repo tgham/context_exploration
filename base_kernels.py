2af629095d47788f6e7b41be9f7eb094f163cf7a
Tom Graham <tgraham1998@outlook.com>
2024-11-22 18:53:44 +0100

    flexible kernel params

--- base_kernels.py
+++ base_kernels.py
@@ -4,52 +4,28 @@ from scipy.spatial.distance import cdist
 
 ### define the kernels
 class BaseKernels:
-    def __init__(self, locations, params):
+    def __init__(self, locations):
         self.locations = locations
         self.N = len(locations)
-        
-        ## set the kernel parameters
-        if params is None:
-            self.c = 1
-            self.scale = 1
-            self.theta = 0
-            # self.theta = np.pi/3
-            self.sigma_f = 2
-            # self.length_scale = self.N/5
-            self.length_scale = 1
-            self.period = self.N/5
-            self.periodic_length_scale = self.N/2
-            self.periodic_theta = np.pi/3
-            self.expl_beta = 0.
-        else:
-            self.c = params[0]
-            self.scale = params[1]
-            self.theta = params[2]
-            self.sigma_f = params[3]
-            self.length_scale = params[4]
-            self.periodic_length_scale = params[5]
-            self.period = params[6]
-            self.periodic_theta = params[7]
 
     ## linear kernels
     
     # linear kernel over x,y, i.e. similarity as a function of the distance from the origin (0,0)
-    def linear(self):
+    def linear(self, c=1):
         dists = np.sqrt(self.locations[:, 0]**2 + self.locations[:, 1]**2)
-        K = np.outer(dists, dists) + self.c
+        K = np.outer(dists, dists) + c
         return K
 
     # linear kernel over x-distance (0) or y-distance (1), i.e. similarity as a function of the distance from (0,:) or (:,0), where the basis vectors are determined by the angle theta (in radians)    
-    def linear_1D(self, dim = 0):
+    def linear_1D(self, dim=0, theta=0, scale=1, c=1):
 
         # define basis vectors
-        rotation = np.array([[np.cos(self.theta), -np.sin(self.theta)], [np.sin(self.theta), np.cos(self.theta)]])
+        rotation = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
         rotated_locations = np.dot(self.locations, rotation)
-        # dists = np.subtract.outer(rotated_locations[:, dim], rotated_locations[:, dim])
 
         # calculate distances
-        dists = rotated_locations[:,dim] * self.scale**2
-        K = np.outer(dists, dists) + self.c
+        dists = rotated_locations[:, dim] * scale**2
+        K = np.outer(dists, dists) + c
         return K
 
 
@@ -57,28 +33,28 @@ class BaseKernels:
     ## RBFs 
 
     # RBF kernel over x,y (i.e. Euclidean distance)
-    def rbf(self):
+    def rbf(self, sigma_f=2, length_scale=0.5):
         dists = cdist(self.locations, self.locations, metric='euclidean')
-        K = self.sigma_f**2 * np.exp(-0.5 * (dists / self.length_scale)**2)
+        K = sigma_f**2 * np.exp(-0.5 * (dists / length_scale)**2)
         return K
 
     # RBF kernel over just x-distance or y-distance
-    def rbf_1D(self, dim=0):
+    def rbf_1D(self, dim=0, theta=0, sigma_f=2, length_scale=0.5):
 
         # define basis vectors
-        rotation = np.array([[np.cos(self.theta), -np.sin(self.theta)], [np.sin(self.theta), np.cos(self.theta)]])
+        rotation = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
         rotated_locations = np.dot(self.locations, rotation)
 
         # calculate distances
         dists = np.subtract.outer(rotated_locations[:, dim], rotated_locations[:, dim])
-        K = self.sigma_f**2 * np.exp(-0.5 * (dists / self.length_scale)**2)
+        K = sigma_f**2 * np.exp(-0.5 * (dists / length_scale)**2)
         return K
 
 
     ## periodic kernel
-    def periodic(self, dim=0):
-        rotation = np.array([[np.cos(self.periodic_theta), -np.sin(self.periodic_theta)], [np.sin(self.periodic_theta), np.cos(self.periodic_theta)]])
+    def periodic(self, dim=0, sigma_f=2, period=1, periodic_length_scale=0.5, periodic_theta=np.pi/3):
+        rotation = np.array([[np.cos(periodic_theta), -np.sin(periodic_theta)], [np.sin(periodic_theta), np.cos(periodic_theta)]])
         rotated_locations = np.dot(self.locations, rotation)
         dists = np.subtract.outer(rotated_locations[:, dim], rotated_locations[:, dim])
-        K = self.sigma_f**2 * np.exp(-2 * np.sin(np.pi * dists / self.period)**2 / self.periodic_length_scale**2)
+        K = sigma_f**2 * np.exp(-2 * np.sin(np.pi * dists / period)**2 / periodic_length_scale**2)
         return K
