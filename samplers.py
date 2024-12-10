38baac15c6d8062f4a9ed8dbe8782eba48520626
Tom Graham <tgraham1998@outlook.com>
2024-12-10 10:30:41 +0100

    acceptance ratio aligns with Arthur's

--- samplers.py
+++ samplers.py
@@ -20,6 +20,8 @@ class GridSampler:
         self.obs = obs
         if self.obs is None:
             self.obs = np.array([])
+        else:
+            self.obs = [(int(i), int(j), float(cost)) for (i, j, cost) in self.obs]
         self.N = N
         self.high_cost = -0.9
         self.low_cost = -0.1
@@ -37,88 +39,86 @@ class GridSampler:
         self.observed_cols = [j for j in range(self.N) if len(self.col_to_obs[j]) > 0]
 
 
+    def sample_obs(self):
+        """Select a random observation from the data."""
+        return self.obs[np.random.randint(len(self.obs))]
+    
     def proposal_params(self, index, is_row):
         """
         Calculate the Beta parameters for the proposal distribution.
         """
         # Get relevant observations for this row/column
         rel_obs = self.row_to_obs[index] if is_row else self.col_to_obs[index]
-        prior_mean_failure = (
-            self.beta_col / (self.alpha_col + self.beta_col) if is_row else self.beta_row / (self.alpha_row + self.beta_row)
+        prior_mean_failure = 1-(
+            self.beta_col / (2*(self.alpha_col + self.beta_col)) if is_row else self.beta_row / (2*(self.alpha_row + self.beta_row))
         )
 
         # Count occurrences of each cost
         m = sum(-cost for (_, _, cost) in rel_obs if cost == self.high_cost)
-        n = (1 - prior_mean_failure) * sum(-cost for (_, _, cost) in rel_obs if cost == self.low_cost)
-
-
+        n = prior_mean_failure * sum(-cost for (_, _, cost) in rel_obs if cost == self.low_cost)
     
         # Update Beta parameters based on observed data
         alpha_prop = self.alpha_row + m if is_row else self.alpha_col + m
         beta_prop = self.beta_row + n if is_row else self.beta_col + n
 
-        return alpha_prop, beta_prop
+        return alpha_prop, beta_prop, m, n
 
     def propose(self, alpha, beta):
         """
         Sample from a Beta distribution with given alpha and beta parameters.
         """
-        return np.random.beta(alpha, beta)
+        return np.random.beta(alpha, beta)    
 
-    def compute_likelihood(self, row_prob, col_prob, obs):
-        """Compute likelihood for a set of observations"""
+    def calculate_likelihood(self, sampled_i, sampled_j, rel_obs, row_p, col_q):
         likelihood = 1.0
-        for (i, j, cost) in obs:
-            p = row_prob * col_prob
-            likelihood *= p if cost == self.high_cost else (1 - p)
+        for o in rel_obs:
+            if o[0] == sampled_i:
+                rel_p = row_p
+                rel_q = self.col_probs[o[1]]
+            elif o[1] == sampled_j:
+                rel_p = self.row_probs[o[0]]
+                rel_q = col_q
+            else:
+                raise ValueError("Observation does not match row or column.")
+            rel_cost = o[2]
+            prob_tmp = rel_p * rel_q
+            likelihood *= prob_tmp**(rel_cost == self.high_cost) * (1 - prob_tmp)**(rel_cost == self.low_cost)
         return likelihood
-    
-    def compute_acceptance_ratio(self, proposed_prob, current_prob, other_probs, obs, is_row, alpha, beta):
-        """Compute the full acceptance ratio including proposal distributions"""
-        # Compute likelihoods
-        if is_row:
-            proposed_likes = [self.compute_likelihood(proposed_prob, q, obs) for q in other_probs]
-            current_likes = [self.compute_likelihood(current_prob, q, obs) for q in other_probs]
-        else:
-            proposed_likes = [self.compute_likelihood(p, proposed_prob, obs) for p in other_probs]
-            current_likes = [self.compute_likelihood(p, current_prob, obs) for p in other_probs]
-        
-        # Proposal densities
-        forward_density = scipy.stats.beta.pdf(proposed_prob, alpha, beta)
-        backward_density = scipy.stats.beta.pdf(current_prob, alpha, beta)
-        
-        # Full ratio
-        return (np.prod(proposed_likes) * backward_density) / (np.prod(current_likes) * forward_density)
 
     def update(self):
         """
         Perform a Metropolis-Hastings update for rows and columns with observations.
         """
-        # Update observed row probabilities
-        for i in self.observed_rows:
-            current_prob = self.row_probs[i]
-            alpha, beta = self.proposal_params(i, is_row=True)
-            proposed_prob = self.propose(alpha, beta)
 
-            ratio = self.compute_acceptance_ratio(
-                proposed_prob, current_prob, self.col_probs, self.row_to_obs[i],
-                is_row=True, alpha=alpha, beta=beta
-            )
-            if np.random.random() < min(1, ratio):
-                self.row_probs[i] = proposed_prob
+        ## sample an observation at random
+        sampled_i, sampled_j, cost = self.sample_obs()
+        current_p = self.row_probs[sampled_i]
+        current_q = self.col_probs[sampled_j]
 
-        # Update observed column probabilities
-        for j in self.observed_cols:
-            current_prob = self.col_probs[j]
-            alpha, beta = self.proposal_params(j, is_row=False)
-            proposed_prob = self.propose(alpha, beta)
+        ## generate proposals
+        alpha_p, beta_p, m1, n1 = self.proposal_params(sampled_i, is_row=True)
+        alpha_q, beta_q, m2, n2 = self.proposal_params(sampled_j, is_row=False)
+        proposed_p = self.propose(alpha_p, beta_p)
+        proposed_q = self.propose(alpha_q, beta_q)
 
-            ratio = self.compute_acceptance_ratio(
-                proposed_prob, current_prob, self.row_probs, self.col_to_obs[j],
-                is_row=False, alpha=alpha, beta=beta
-            )
-            if np.random.random() < min(1, ratio):
-                self.col_probs[j] = proposed_prob
+        ## subselect relevant obs (i.e. those containing i or j)
+        rel_obs = [(i_, j_, cost_) for (i_, j_, cost_) in self.obs if (i_ == sampled_i) or (j_ == sampled_j)]
+
+        ## calculate likelihoods
+        likelihood_num = self.calculate_likelihood(sampled_i, sampled_j, rel_obs, proposed_p, proposed_q)
+        likelihood_den = self.calculate_likelihood(sampled_i, sampled_j, rel_obs, current_p, current_q)
+
+        ## calculate prior * transition prob terms
+        prior_num = proposed_p**m1 * (1 - proposed_p)**n1 * proposed_q**m2 * (1 - proposed_q)**n2
+        prior_den = current_p**m1 * (1 - current_p)**n1 * current_q**m2 * (1 - current_q)**n2
+
+        ## calculate acceptance ratio
+        epsilon = 1e-10
+        acceptance_ratio = (likelihood_num * prior_num) / (likelihood_den * prior_den + epsilon)
+        if np.random.random() < min(1, acceptance_ratio):
+            self.row_probs[sampled_i] = proposed_p
+            self.col_probs[sampled_j] = proposed_q
+
 
     def sample(self, n_iter=1000):
         """Run the sampler for a specified number of iterations."""
