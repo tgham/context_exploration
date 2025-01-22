ecd9c7e16d0a2792c3499e44f27441395a3f3b0f
Tom Graham <tgraham1998@outlook.com>
2025-01-22 17:59:37 +0100

    allegedly quicker

--- value_iteration.py
+++ value_iteration.py
@@ -4,78 +4,53 @@ from utils import argm
 
 ## value iteration
 @njit
-def value_iteration(dp_costs, goal, max_iters = 1000, theta = 0.0001, discount = 0.99):
-
+def value_iteration(dp_costs, goal, max_iters=1000, theta=0.0001, discount=0.99):
     N = len(dp_costs)
     n_actions = 4
 
-    ## init actions
+    # Action directions
     action_directions = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
-    
-    ## init tables
+
+    # Initialize tables
     V = np.zeros((N, N))
-    A = np.zeros((N, N))
-    Q = np.zeros((N, N, n_actions))
+    A = np.zeros((N, N), dtype=np.int32)
+    Q = np.full((N, N, n_actions), np.nan)
 
-    ## determine whether to use true costs or inferred costs
-    # if self.known_costs:
-    #     dp_costs = self.costs.copy()
-    # else:
-    #     dp_costs = self.posterior_mean.reshape(self.N, self.N).copy()
+    # Set cost of the goal to 0
+    goal_x, goal_y = goal
 
-    ## set cost of goal to 0
-    # dp_costs[goal[0], goal[1]] = 0
-
-    # assert np.all(dp_costs <= 0), 'costs are not all negative: {}'.format(dp_costs)
-
-    ## loop through states
     for i in range(max_iters):
         delta = 0
+
         for x in range(N):
             for y in range(N):
-                
-                ## skip if at goal state to ensure that v(goal) = 0
-                if x == goal[0] and y == goal[1]:
-                    # V[x, y] = 0
+                if x == goal_x and y == goal_y:
                     continue
 
                 v = V[x, y]
-                # q = np.zeros(n_actions)
-                q = np.full(n_actions, np.nan)
 
-                ## loop through actions and get the discounted value of each of the next states
+                # Compute Q-values for all valid actions in one loop
                 for a in range(n_actions):
-
-                    ## allow wall moves
-                    # next_state = np.clip([x, y] + self._action_to_direction[a], 0, self.N-1)
-                    # q[a] = dp_costs[next_state[0], next_state[1]] + discount*V[next_state[0], next_state[1]]
-
-                    ## or, don't allow wall moves
                     next_x = x + action_directions[a][0]
                     next_y = y + action_directions[a][1]
+
                     if 0 <= next_x < N and 0 <= next_y < N:
-                        q[a] = dp_costs[next_x, next_y] + discount * V[next_x, next_y]
+                        Q[x, y, a] = dp_costs[next_x, next_y] + discount * V[next_x, next_y]
+                    else:
+                        Q[x, y, a] = np.nan
 
-                ## update the Q-table
-                Q[x, y] = q
+                # Update value and action tables
+                max_q = np.nanmax(Q[x, y])
+                V[x, y] = max_q
+                best_actions = np.where(Q[x, y] == max_q)[0]
+                A[x, y] = np.random.choice(best_actions)
 
-                ## use the best action to update the value of the current state
-                V[x, y] = np.nanmax(q)
+                # Update convergence threshold
+                delta = max(delta, abs(v - max_q))
 
-                idx = np.where(q == np.nanmax(q))[0]
-                A[x, y] = np.random.choice(idx)
-
-                ## check if converged
-                delta = max(delta, np.abs(v - V[x, y]))
-        
         if delta < theta:
-            # print('DP converged after {} iterations'.format(i))
             break
 
-        if i == max_iters-1:
-            print('DP did not converge after '+str(i)+ ' iterations')
-
-    ## need to check if this has lead to a valid policy
-
     return V, Q, A
+
 
